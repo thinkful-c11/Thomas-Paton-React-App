d3192920ed008bdeb76068165751984e878c0acc
tckastanek <tckastanek@gmail.com>
2017-06-21 14:11:58 -0400

    added guesses and positioning

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,72 @@
 import React from 'react';
-import NumberOutput from './number-output'
+import Output from './Output';
+import './App.css';
+
 
 export default class App extends React.Component {
-    constuctor(props){
-        super(props);
-        this.state = {
-            computerNumber: 0,
-            playerNumber: 0,
-            upper: 100,
-            lower: 1,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      numbers: [1, 2, 4, 10, 5, 8, 11, 9],
+      guesses: []
+    };
+  }
+
+  compareNumbers(guess, currentNum, nextNum) {
+    function greaterThan() {
+      return nextNum > currentNum;
+    }
+    function lessThan() {
+      return nextNum < currentNum;
     }
 
-    generateComputerNumber() {
-        const newNumber = Math.floor((Math.random() * 100) + 1);
-        this.setState({
-            computerNumber: newNumber
-        })
+    if ((guess === 'higher') ? greaterThan() : lessThan()) {
+      this.setState({ guesses: [...this.state.guesses, true] });
+    } else {
+      this.setState({ guesses: [...this.state.guesses, false] });
     }
-
-    generatePlayerNumber(upper, lower) {
-        const newNumber = Math.floor((Math.random() * upper) + lower);
-        this.setState({
-            playerNumber: newNumber
-        })
-    }
+  }
 
 
-    render () {
-        return(
-            <div>
-                <button onClick={() => {this.generateComputerNumber(); this.generatePlayerNumber(this.state.upper, this.state.lower);} }>Start New Game</button>
-                <NumberOutput />
-            </div>
-        )
-    }
+
+  render() {
+    const stateGuess = this.state.guesses;
+    const gameResult = function() {
+      if (stateGuess.length === 0) {
+        return '';
+      }
+      else if (stateGuess[stateGuess.length - 1] === true) {
+        return 'Correct!';
+      } else {
+        return 'Wrong!';
+      }
+    };
+    const gamePosition = stateGuess.length;
+    return (
+      <div className='app'>
+        {/* <button onClick={() => console.log('clicked')}>Start Game!</button> */}
+        <Output value={this.state.numbers[0]} />
+        <button onClick={() => this.compareNumbers('higher', this.state.numbers[gamePosition], this.state.numbers[gamePosition + 1])}>Higher</button>
+        <button onClick={() => this.compareNumbers('lower', this.state.numbers[gamePosition], this.state.numbers[gamePosition + 1])}>Lower</button>
+        <Output value={gameResult()} />
+      </div>
+    );
+  }
 }
+
+
+
+
+    // generateComputerNumber() {
+    //     const newNumber = Math.floor((Math.random() * 100) + 1);
+    //     this.setState({
+    //         computerNumber: newNumber
+    //     })
+    // }
+    //
+    // generatePlayerNumber(upper, lower) {
+    //     const newNumber = Math.floor((Math.random() * upper) + lower);
+    //     this.setState({
+    //         playerNumber: newNumber
+    //     })
+    // }
---
+++ src/components/Output.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Output(props) {
+  return (
+    <output>{props.value}</output>
+  );
+}
