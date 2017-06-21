e0b4a3c9a60836217dfcfea53f82c0c62e0e9cbe
tckastanek <tckastanek@gmail.com>
2017-06-21 15:46:36 -0400

    added new game render for You Won condition

--- src/components/ButtonControls.js
+++ src/components/ButtonControls.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 export default function ButtonControls(props) {
   const testArray =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  if (props.stateCards.length === 0) {
-    return (
-      <button onClick={() => props.newGame(testArray)}>New Game</button>
-    );
-  } else if (props.gameResult === 'Game Over!') {
+  if (props.gameResult === 'You Won!' || props.stateCards.length === 0 || props.gameResult === 'Game Over!') {
     return (
       <button onClick={() => props.newGame(testArray)}>New Game</button>
     );
